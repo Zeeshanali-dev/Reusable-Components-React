69e671a7749030b855403c71c0d0f5c22b40baf1
Test Project <meharshani963@gmail.com>
2024-07-12 23:10:51 +0500

    helper method

---
+++ src/Components/Axios/axiosSetup.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const Api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const FormDataAPI = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+export const getRequest = async (data) => {
+  const { route } = data;
+  const response = await Api.get(route);
+
+  return response;
+};
+
+export const getRequestWithParams = async (data) => {
+  const { route, formValues } = data;
+  const response = await Api.get(route, { params: formValues });
+
+  return response;
+};
+
+export const postRequest = async (data) => {
+  const { route, formValues } = data;
+  const response = await Api.post(route, formValues);
+
+  return response;
+};
+
+export const postRequestWithFormData = async (data) => {
+  const { route, formValues } = data;
+  const response = await FormDataAPI.post(route, formValues);
+
+  return response;
+};
